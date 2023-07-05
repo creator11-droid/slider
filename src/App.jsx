72dd3c7724facad2ce733608111b77e726bc591b
Atole Kester <atolekester@gmail.com>
2023-07-05 15:57:40 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Carousels from "./Carousels";
+import SlickCarousels from "./SlickCarousels";
+
+function App() {
+  return (
+    <main>
+      <Carousels />
+      <SlickCarousels />
+    </main>
+  );
+}
+
+export default App;
